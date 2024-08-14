9edaa53304e3e50f38156a9f56179fca9de4ef27
Xiangbo Cai <155998618+caishamble@users.noreply.github.com>
2024-08-13 20:16:07 -0400

    Update ttl_74161_tb.v
    testbench part verilog update

--- ttl_74161.srcs/sim_1/new/ttl_74161_tb.v
+++ ttl_74161.srcs/sim_1/new/ttl_74161_tb.v
@@ -24,47 +24,54 @@
 
 
 module ttl_74161_tb;
- // Inputs
- reg ld, cet, cep, clk, clr;
- reg [3:0] p;
- // Outputs
- wire [3:0] q;
- wire tc;
- // Instantiate the module under test
- ttl_74161 uut (
- .ld(ld),
- .p(p),
- .q(q),
- .cet(cet),
- .cep(cep),
- .clk(clk),
- .tc(tc),
- .clr(clr)
- );
- // Clock generation
- always begin
- #5 clk = ~clk;
- end
- // Reset
- initial begin
- ld = 0;
- p = 4'b0000;
- clk = 0;
- clr = 0;
- cep = 0;
- cet = 0;
- end
- // Test scenarios
- initial begin
- // Scenario 1: clr = 0
- #15 clr = 1;
- #15 clr = 0;
- #15 p = 3;
- // Scenario 2: clr = 1, ld = 1
- #15 ld = 1;
- // Scenario 3: clr = 1, ld = 1, cep = 1, cet = 1
- # 15 cep = 1; cet = 1;
- // End simulation
- #350 $finish;
- end
+
+    // Inputs
+    reg ld, cet, cep, clk, clr;
+    reg [3:0] p;
+
+    // Outputs
+    wire [3:0] q;
+    wire tc;
+
+    // Instantiate the module under test
+    ttl_74161 uut (
+        .ld(ld),
+        .p(p),
+        .q(q),
+        .cet(cet),
+        .cep(cep),
+        .clk(clk),
+        .tc(tc),
+        .clr(clr)
+    );
+
+    // Clock generation
+    always begin
+        #5 clk = ~clk;
+    end
+    // Reset
+    initial begin
+        ld = 0;
+        p = 4'b0000;
+        clk = 0;
+        clr = 0;
+        cep = 0;
+        cet = 0;
+    end
+    
+    // Test scenarios
+    initial begin
+        // Scenario 1: clr = 0
+        #15 clr = 1;
+
+        #15 p = 3;
+        // Scenario 2: clr = 1, ld = 1
+        #15 ld = 1;
+
+        // Scenario 3: clr = 1, ld = 1, cep = 1, cet = 1
+        # 15 cep = 1; cet = 1;
+
+        // End simulation
+        #350 $finish;
+    end
 endmodule
